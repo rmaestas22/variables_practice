91848a2d0a3d596a62d1c3dbf3240e782cc5954a
Ray Maestas <rjmaestas1991@gmail.com>
2022-06-07 20:48:17 -0600

    add puts statements reassigned variable values

--- variables.rb
+++ variables.rb
@@ -1,6 +1,7 @@
 email = "email@email.com"
 email_1 = "email@gmail.com"
 
+
 current_age = 30
 future_age = 31
 
@@ -10,3 +11,12 @@ amount_due = 5.5
 
 payment_made = true
 payment_complete = false
+
+email_1 = "raymail@email.com"
+
+current_age = "30"
+puts email_1 
+puts amount_due
+puts current_age
+puts payment_made
+puts future_age
